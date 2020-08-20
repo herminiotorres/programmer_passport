7f58f44e3318d79b90bda8b0217a538a08a205a0
Herminio Torres <herminiocesar@gmail.com>
2020-08-20 08:53:19 -0300

    LiveView course: add handle events to keeps the lifecycle for a live_view, so now it could be add, remove, inc and dec a new counter

--- 2020/liveview/turbo_counter/lib/turbo_counter/counter.ex
+++ 2020/liveview/turbo_counter/lib/turbo_counter/counter.ex
@@ -7,6 +7,25 @@ defmodule TurboCounter.Counter do
     Map.put(counters, name, value)
   end
 
+  def add_counter(counters) do
+    Map.put(counters, max_counter_name(counters), 0)
+  end
+
+  defp max_counter_name(counters) do
+    counters
+    |> Map.keys
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.max
+    |> Kernel.+(1)
+    |> to_string
+  rescue
+    _exception -> "1"
+  end
+
+  def remove_counter(counters, name) do
+    Map.delete(counters, name)
+  end
+
   def inc(counters, name) do
     Map.put(counters, name, counters[name] + 1)
   end
@@ -15,7 +34,7 @@ defmodule TurboCounter.Counter do
     Map.put(counters, name, counters[name] - 1)
   end
 
-  def remove_counter(counters, name) do
-    Map.delete(counters, name)
+  def clear(counters, name) do
+    Map.put(counters, name, 0)
   end
 end
--- 2020/liveview/turbo_counter/lib/turbo_counter_web/live/count_live.ex
+++ 2020/liveview/turbo_counter/lib/turbo_counter_web/live/count_live.ex
@@ -3,12 +3,11 @@ defmodule TurboCounterWeb.CountLive do
   alias TurboCounter.Counter
 
   def mount(_params, _session, socket) do
-    :timer.send_interval(1000, self(), :tick)
+    # :timer.send_interval(1000, self(), :tick)
     {
       :ok,
       socket
       |> new
-      |> add_counter(:count, 0)
     }
   end
 
@@ -16,26 +15,63 @@ defmodule TurboCounterWeb.CountLive do
     assign(socket, counters: Counter.new())
   end
 
-  defp add_counter(socket, name, count) do
-    assign(
-      socket,
-      counters: Counter.add_counter(socket.assigns.counters, name, count)
-    )
-  end
-
   def render(assigns) do
     ~L"""
     <h1>Welcome to Turbo Counter</h1>
     <h2>If you dream it, we can count it.</h2>
-    <p>Count: <%= @counters.count %></p>
+    <hr>
+    <%= for {name, count} <- @counters do %>
+      <p>
+        Counter <%= name %>: <%= count %>
+        <button phx-click="inc" phx-value-counter="<%= name %>">inc</button>
+        | <button phx-click="dec" phx-value-counter="<%= name %>">dec</button>
+        | <button phx-click="clear" phx-value-counter="<%= name %>">clear</button>
+        | <button phx-click="remove" phx-value-counter="<%= name %>">remove</button>
+      </p>
+    <% end %>
+    <button phx-click="add">add counter</button>
     """
   end
 
-  def handle_info(:tick, socket) do
-    {:noreply, count(socket)}
+  defp inc(socket, counter) do
+    assign(socket, counters: Counter.inc(socket.assigns.counters, counter))
   end
 
-  defp count(socket) do
-    assign(socket, counters: Counter.inc(socket.assigns.counters, :count))
+  defp dec(socket, counter) do
+    assign(socket, counters: Counter.dec(socket.assigns.counters, counter))
+  end
+
+  defp clear(socket, counter) do
+    assign(socket, counters: Counter.clear(socket.assigns.counters, counter))
+  end
+
+  defp add_counter(socket) do
+    assign(
+      socket,
+      counters: Counter.add_counter(socket.assigns.counters)
+    )
+  end
+
+  defp remove_counter(socket, counter) do
+    assign(
+      socket,
+      counters: Counter.remove_counter(socket.assigns.counters, counter)
+    )
+  end
+
+  def handle_event("inc", %{"counter" => counter}, socket) do
+    {:noreply, inc(socket, counter)}
+  end
+  def handle_event("dec", %{"counter" => counter}, socket) do
+    {:noreply, dec(socket, counter)}
+  end
+  def handle_event("clear", %{"counter" => counter}, socket) do
+    {:noreply, clear(socket, counter)}
+  end
+  def handle_event("add", _params, socket) do
+    {:noreply, add_counter(socket)}
+  end
+  def handle_event("remove", %{"counter" => counter}, socket) do
+    {:noreply, remove_counter(socket, counter)}
   end
 end
